3d2440b47a8e70566641a6a6f0c8dfc1796501f0
Ridhima112 <ridhimasaxena061@gmail.com>
2026-06-05 15:38:15 +0530

    Add files via upload

---
+++ Class_Work/List_5th_june/consecutiveNumberDectector.py
@@ -0,0 +1,8 @@
+#program to find all pairs of consecutive number
+numbers = [4, 5, 6, 10, 11, 15, 16, 17]
+
+print("Consecutive pairs:")
+
+for i in range(len(numbers) - 1):
+    if numbers[i + 1] - numbers[i] == 1:
+        print(numbers[i], numbers[i + 1])
