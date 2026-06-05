875e2d8db6416caf4cd0eabcbbc82b71f5e69206
Ridhima112 <ridhimasaxena061@gmail.com>
2026-06-05 22:58:49 +0530

    Add files via upload

---
+++ Class_Work/iteration-4th_june/attendence_counter.py
@@ -0,0 +1,8 @@
+#program that counts the number of studends entering the classroom and displays the attendance count 
+student_count = 0
+
+while(student_count < 30):
+    student_count+=1
+    print("total student=",student_count)
+#---------------------------------------
+print("All the students have entered the classroom")
